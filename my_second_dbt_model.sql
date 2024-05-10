9edc08482b625407aa7669e581de6b477a8065cc
Christopher Piquette <chpiquette@gmail.com>
2024-05-10 12:11:25 -0400

    Update my_second_dbt_model.sql

--- my_second_dbt_model.sql
+++ my_second_dbt_model.sql
@@ -1,6 +1,8 @@
 
--- Use the `ref` function to select from other models
+-- Create a talbe using the `ref` function to select from other models, filtering only for 'US' data.
+
+{{ config(materialized='table') }}
 
 select *
-from {{ ref('my_first_dbt_model') }}
-where id = 1
+from {{ ref('users') }}
+where country_code = 'US'
