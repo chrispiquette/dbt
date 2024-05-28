0b7fc743f929d13b4d8c8ca11a1bd7248d1e6ccb
Christopher Piquette <chpiquette@gmail.com>
2024-05-28 17:28:48 -0400

    minor change

--- users_country_conversion.sql
+++
@@ -1,9 +0,0 @@
--- Create a table that inherits our 'users' data and using a Macro adds a column 
--- converting country_code to country name.
-
--- Table statement:
-{{ config(materialized='table') }}
-
--- Inherit data from 'users' model and convert country code to country_name:
-select *, {{ CountryCodeToCountryName('country_code') }} as country_name
-from {{ ref('users') }}
--- users_usa.sql
+++
@@ -1,8 +0,0 @@
-
--- Create a talbe using the `ref` function to select from other models, filtering only for 'US' data.
-
-{{ config(materialized='table') }}
-
-select *
-from {{ ref('users') }}
-where country_code = 'US'
